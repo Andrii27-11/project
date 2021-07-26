8274b057e036797c3ef75086b15dc87b441fcb82
Andrii <example@gmail.com>
2021-07-26 15:06:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+//alert ('Hello 24 y!');
+
+// const conf = confirm('Are you here?');
+// console.log(conf);
+
+// const answer = +prompt("Skolko vam let?", "");
+// console.log(answer + 20);
+// console.log(typeof answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Vashe imya?', '');
+// answers[1] = prompt('Vasha familia?', '');
+// answers[2] = prompt('Skolko let?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5/wwdda/dcs`);
+
+// const user = 'Andrii!';
+
+// alert(`Nice to see you, ${user}`);
+
+
+
+let incr = 10,
+decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+
+console.log(8%3);
